fff741521e59b64a3ff403368795527f77df72a6
MustaphaBousilDev <bousilmustapha@gmail.com>
2025-11-14 00:48:52 +0100

    add script python for processing data from fake data json for simulation the workflow

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,17 +1,55 @@
 provider "aws" {
-    region = var.aws_region
+  region = var.aws_region
 }
 
 resource "aws_s3_bucket" "data_bucket" {
-    bucket = "${var.bucket_name}-${random_id.bucket_suffix.hex}"
+  bucket = "${var.bucket_name}-${random_id.bucket_suffix.hex}"
 }
 
 resource "aws_kinesis_stream" "data_stream" {
-    name             = var.stream_name
-    shard_count      = var.shard_count
-    retention_period = var.retention_period
+  name             = var.stream_name
+  shard_count      = var.shard_count
+  retention_period = var.retention_period
 }
 
 resource "random_id" "bucket_suffix" {
-    byte_length = 4
+  byte_length = 4
+}
+
+resource "aws_iam_role" "lambda_execution_role" {
+  name = "data-processing-lambda-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+  role       = aws_iam_role.lambda_execution_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_kinesis_execution" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaKinesisExecutionRole"
+  role       = aws_iam_role.lambda_execution_role.name
+}
+
+
+
+
+resource "aws_lambda_function" "data_processing_lambda" {
+  filename         = "lambda_function.zip"
+  function_name    = "data-processing-lambda"
+  role             = aws_iam_role.lambda_execution_role.arn
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.9"
+  source_code_hash = filebase64sha256("lambda_function.zip")
 }
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,24 +1,24 @@
 variable "aws_region" {
-    description = "The AWS region to deploy resources in"
-    default     = "us-west-2"
+  description = "The AWS region to deploy resources in"
+  default     = "us-east-1"
 }
 
 variable "bucket_name" {
-    description = "The name of the S3 bucket"
-    default     = "my-unique-data-bucket-20230606"
+  description = "The name of the S3 bucket"
+  default     = "my-unique-data-bucket-20230606"
 }
 
 variable "stream_name" {
-    description = "The name of the Kinesis Data Stream"
-    default     = "my-data-stream"
+  description = "The name of the Kinesis Data Stream"
+  default     = "my-data-stream"
 }
 
 variable "shard_count" {
-    description = "The number of shards for the Kinesis Data Stream"
-    default     = 1
+  description = "The number of shards for the Kinesis Data Stream"
+  default     = 1
 }
 
 variable "retention_period" {
-    description = "The retention period (in hours) for the Kinesis Data Stream"
-    default     = 24
+  description = "The retention period (in hours) for the Kinesis Data Stream"
+  default     = 24
 }
